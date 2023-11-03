dca799312dc2e299bb0185da36d18807bf15255d
TM <thfo.wt.h.y.xxx@gmail.com>
2023-11-03 21:17:38 +0900

    update

---
+++ abc292/a/main.cpp
@@ -0,0 +1,34 @@
+#include <algorithm>
+#include <cmath>
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+#define rep(i, m, n) for (int i = (int)(m); i < (int)(n); ++i)
+#define rrep(i, m, n) for (int i = (int)(m); i > (int)(n); --i)
+
+template <typename T, typename U>
+pair<T, U> tp(T a, U b) {
+    return std::make_pair(a, b);
+}
+
+constexpr int MOD = 1000000007;
+
+int main() {
+    return 0;
+}
